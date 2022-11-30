43f2e44de23513e7d05cdcf2c3a494fc7582fe78
anastasovv <simon@digitalnode.com>
2022-11-30 21:54:29 +0100

    Add #demo container to code, to display coordinates, add mariadb dependency in pom xml

--- Domasna2/src/main/java/com/example/diansspring/web/HomeController.java
+++ Domasna2/src/main/java/com/example/diansspring/web/HomeController.java
@@ -3,13 +3,14 @@ package com.example.diansspring.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping
+    @PostMapping
     public String getHomePage() {
         return "home";
     }
