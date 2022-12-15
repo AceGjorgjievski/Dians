5285515ebfc0dd5472d73066b7dc5c7b19729573
anastasovv <simon@digitalnode.com>
2022-12-15 01:42:52 +0100

    Add captcha to login page

--- Domasna3/src/main/java/com/example/diansspring/web/controller/LoginController.java
+++ Domasna3/src/main/java/com/example/diansspring/web/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.example.diansspring.web.controller;
 
 
+import cn.apiclub.captcha.Captcha;
 import com.example.diansspring.model.User;
 import com.example.diansspring.model.exceptions.InvalidUserCredentialsException;
 import com.example.diansspring.service.AuthService;
+import com.example.diansspring.util.CaptchaUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,29 +24,22 @@ public class LoginController {
     }
 
     @GetMapping
-    public String getLogPage(Model model) {
+    public String getLoginPage(Model model) {
         model.addAttribute("pageTitle", "Login - Findify");
         model.addAttribute("mainCssFile", "login.css");
         model.addAttribute("mainBodyContent", "login");
 
+        User user = new User();
+        getCaptcha(user);
+        model.addAttribute("captchaDetailsUser", user);
+
         return "master-template";
     }
 
-    @PostMapping
-    public String login(HttpServletRequest request, Model model) {
-        User user = null;
-        try {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-//            user = this.authService.login(username, password);
-
-            request.getSession().setAttribute("user",user);
-            return "redirect:/home";
-        } catch (InvalidUserCredentialsException e) {
-            model.addAttribute("hasError", true);
-            model.addAttribute("error", e.getMessage());
-            return "login";
-        }
-
+    private void getCaptcha(User user) {
+        Captcha captcha = CaptchaUtil.createCaptcha(240, 70);
+        user.setHiddenCaptcha(captcha.getAnswer());
+        user.setCaptcha("");
+        user.setRealCaptcha(CaptchaUtil.encodeCaptcha(captcha));
     }
 }
