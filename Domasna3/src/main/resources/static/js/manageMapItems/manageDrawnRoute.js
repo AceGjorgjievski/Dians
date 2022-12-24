8f723ebbc2935296fd475109b982293fa0339faa
anastasovv <simon@digitalnode.com>
2022-12-24 09:40:05 +0100

    Add information about route to the destination

--- Domasna3/src/main/resources/static/js/manageMapItems/manageDrawnRoute.js
+++ Domasna3/src/main/resources/static/js/manageMapItems/manageDrawnRoute.js
@@ -24,6 +24,8 @@ export async function drawRoute() {
             GLOBALS.profiles.drawnRoute = L.polyline(coordinates, {color: 'green', weight: 6});
             GLOBALS.profiles.drawnRoute.addTo(GLOBALS.map);
 
+            bindPopupToRoute(data.paths[0].distance, data.paths[0].time);
+
             GLOBALS.profiles.doDrawRoute = true;
         });
 }
@@ -34,5 +36,22 @@ export function removeRoute() {
         GLOBALS.profiles.drawnRoute = undefined;
 
         GLOBALS.profiles.doDrawRoute = false;
+    }
+}
+
+function bindPopupToRoute(distanceInMeters, walkingTimeInMs) {
+    // write info such as
+    // destination: royal burger
+    // distance: 1.3km
+    // walking time: 13mins
+
+    let distanceInKm = distanceInMeters / 1000.0;
+    let walkingTimeInMinutes = walkingTimeInMs / (1000.0 * 60);
+
+    if (GLOBALS.profiles.drawnRoute !== undefined) {
+        GLOBALS.profiles.drawnRoute.bindPopup(document.getElementsByClassName('routePopup')[0]?.outerHTML).openPopup();
+        for (let elem of document.getElementsByClassName('routeDestination')) elem.innerHTML = GLOBALS.profiles.clickedFacility.name;
+        for (let elem of document.getElementsByClassName('routeDistance')) elem.innerHTML = distanceInKm.toFixed(2) + ' km';
+        for (let elem of document.getElementsByClassName('routeWalkingTime')) elem.innerHTML = walkingTimeInMinutes.toFixed(2) + ' min';
     }
 }
